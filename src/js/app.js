709e0082a35f2d438b0dd41074f4dacf9eb924ef
Findlay <findlay12@ymail.com>
2019-10-25 11:21:11 +0200

    did sometin

--- src/js/app.js
+++ src/js/app.js
@@ -5,23 +5,94 @@ export function User (user) {
   this.isLoggedIn = false
 }
 
-const div = document.querySelector('.drag')
+const body = document.querySelector('body')
+const main = document.querySelector('.main')
+console.log(main)
+const footer = document.querySelector('footer')
+footer.addEventListener('click', function onClick (event) {
+  const clickedItem = event.target
+  let div
+  let header
+  if (clickedItem.tagName === 'IMG') {
+    console.log('i am here boi')
+    const name = clickedItem.getAttribute('name')
+    const app = document.createElement(name)
+    let templ = document.getElementById('window')
+    templ = templ.content.cloneNode(true)
+    console.log(app)
+    div = templ.querySelector('.drag')
+    const innerBody = div.querySelector('#innerBody')
+    header = div.querySelector('#top-Bar')
+    innerBody.append(app)
+    main.append(div)
+    div.addEventListener('mousedown', mouseDown)
+  }
+
+  function mouseDown (e) {
+    if (e.target !== header) {
+      return
+    }
+    console.log('urboi is loistening')
+    const rect = div.getBoundingClientRect()
+    const prevX = e.clientX
+    const prevY = e.clientY
+    // console.log(e.target)
+    // console.log(prevX)
+    // console.log(prevY)
+  
+    window.addEventListener('mousemove', mouseMove)
+  
+    function mouseMove (e) {
+      const distanceMovedX = e.clientX - prevX
+      const distanceMovedY = e.clientY - prevY
+      div.style.left = `${rect.x + distanceMovedX}px`
+      div.style.top = `${rect.y + distanceMovedY}px`
+      // console.log('getBoundingrect ', div.getBoundingClientRect())
+      // console.log('X: ', e.clientX)
+      // console.log('Y: ', e.clientY)
+    }
+  
+    window.addEventListener('mouseup', function mouseUp () {
+      this.console.log('final', div.getBoundingClientRect())
+      this.window.removeEventListener('mousemove', mouseMove)
+      this.window.removeEventListener('mousedown', mouseDown)
+      this.window.removeEventListener('mouseup', mouseUp)
+    })
+  }
+})
+
+
+/*
+let templ = document.getElementById('window')
+templ = templ.content.cloneNode(true)
+
+const body = document.querySelector('body')
+console.log(body)
+const div = templ.querySelector('.drag')
+const header = div.querySelector('#top-Bar')
+const chat = document.createElement('chat-app')
+const innerBody = div.querySelector('#innerBody')
+body.append(div)
+innerBody.append(chat)
+console.log(header)
 console.log(div)
 
 div.addEventListener('mousedown', function mouseDown (e) {
+  if (e.target !== header) {
+    return
+  }
   const rect = div.getBoundingClientRect()
-  let prevX = e.clientX
-  let prevY = e.clientY
+  const prevX = e.clientX
+  const prevY = e.clientY
+  console.log(e.target)
   console.log(prevX)
   console.log(prevY)
 
   window.addEventListener('mousemove', mouseMove)
 
   function mouseMove (e) {
-    const newX = e.clientX
-    const newY = e.clientY
-    const distanceMovedX = newX - prevX
-    const distanceMovedY = newY - prevY
+    const distanceMovedX = e.clientX - prevX
+    const distanceMovedY = e.clientY - prevY
     div.style.left = `${rect.x + distanceMovedX}px`
     div.style.top = `${rect.y + distanceMovedY}px`
     console.log('getBoundingrect ', div.getBoundingClientRect())
@@ -39,8 +110,7 @@ div.addEventListener('mousedown', function mouseDown (e) {
 })
 
 
-
-
+*/
 
 
 
